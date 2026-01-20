c7ebb657719300d16033526551b66938ab1904f0
Darren Gavriel <darrengvs.dg@gmail.com>
2026-01-20 09:21:45 +0700

    membuat query untuk mendapatkan lokasi

--- app/Lokasi.php
+++ app/Lokasi.php
@@ -21,4 +21,15 @@ class Lokasi extends Model
     {
         return $this->hasMany(RiwayatTransaksi::class, 'id_lokasi', 'id');
     }
+    public function getLokasi($kode_lokasi = null, $nama_lokasi = null)
+    {
+        $query = self::select('*')
+            ->when($kode_lokasi, function ($query, $kode_lokasi){
+                return $query->orWhere('kode_lokasi', 'like', '%' . $kode_lokasi . '%');
+            })
+            ->when($nama_lokasi, function ($query, $nama_lokasi){
+                return $query->orWhere('nama_lokasi', 'like', '%' . $nama_lokasi . '%');
+            });
+        return $query;
+    }
 }
