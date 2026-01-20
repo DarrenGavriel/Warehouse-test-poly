f6c553c1b3101cfbecdb5bf003ee748ba58168ef
Darren Gavriel <darrengvs.dg@gmail.com>
2026-01-20 09:23:21 +0700

    ganti isinya agar lebih banyak

--- database/seeds/LokasiSeeder.php
+++ database/seeds/LokasiSeeder.php
@@ -11,9 +11,28 @@ class LokasiSeeder extends Seeder
      */
     public function run()
     {
-        Lokasi::create(['nama_lokasi' => 'Gedung A', 'kode_lokasi' => 'GDG-A']);
-        Lokasi::create(['nama_lokasi' => 'Gedung B', 'kode_lokasi' => 'GDG-B']);
-        Lokasi::create(['nama_lokasi' => 'Gedung C', 'kode_lokasi' => 'GDG-C']);
-        Lokasi::create(['nama_lokasi' => 'Gedung D', 'kode_lokasi' => 'GDG-D']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG A', 'kode_lokasi' => 'GDG-A']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG B', 'kode_lokasi' => 'GDG-B']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG C', 'kode_lokasi' => 'GDG-C']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG D', 'kode_lokasi' => 'GDG-D']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG E', 'kode_lokasi' => 'GDG-E']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG F', 'kode_lokasi' => 'GDG-F']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG G', 'kode_lokasi' => 'GDG-G']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG H', 'kode_lokasi' => 'GDG-H']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG I', 'kode_lokasi' => 'GDG-I']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG J', 'kode_lokasi' => 'GDG-J']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG K', 'kode_lokasi' => 'GDG-K']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG L', 'kode_lokasi' => 'GDG-L']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG M', 'kode_lokasi' => 'GDG-M']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG N', 'kode_lokasi' => 'GDG-N']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG O', 'kode_lokasi' => 'GDG-O']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG P', 'kode_lokasi' => 'GDG-P']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG Q', 'kode_lokasi' => 'GDG-Q']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG R', 'kode_lokasi' => 'GDG-R']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG S', 'kode_lokasi' => 'GDG-S']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG T', 'kode_lokasi' => 'GDG-T']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG U', 'kode_lokasi' => 'GDG-U']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG V', 'kode_lokasi' => 'GDG-V']);
+        Lokasi::create(['nama_lokasi' => 'GEDUNG W', 'kode_lokasi' => 'GDG-W']);
     }
-}   
+}
