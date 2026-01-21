f01d39b50f1f713874ff0e014e85856b952c14a1
Darren Gavriel <darrengvs.dg@gmail.com>
2026-01-21 08:18:56 +0700

    mengganti agar nama barang menjadi kapital semua

--- database/seeds/BarangSeeder.php
+++ database/seeds/BarangSeeder.php
@@ -11,9 +11,9 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        Barang::create(['nama_barang' => 'Barang 1', 'kode_barang' => 'BRG1']);
-        Barang::create(['nama_barang' => 'Barang 2', 'kode_barang' => 'BRG2']);
-        Barang::create(['nama_barang' => 'Barang 3', 'kode_barang' => 'BRG3']);
-        Barang::create(['nama_barang' => 'Barang 4', 'kode_barang' => 'BRG4']);
+        Barang::create(['nama_barang' => 'BARANG 1', 'kode_barang' => 'BRG1']);
+        Barang::create(['nama_barang' => 'BARANG 2', 'kode_barang' => 'BRG2']);
+        Barang::create(['nama_barang' => 'BARANG 3', 'kode_barang' => 'BRG3']);
+        Barang::create(['nama_barang' => 'BARANG 4', 'kode_barang' => 'BRG4']);
     }
 }
