6445397236ad4c5bbb5eaa0e1b83e54bfe1df706
Darren Gavriel <darrengvs.dg@gmail.com>
2026-01-21 08:20:16 +0700

    menambahkan navbar untuk master barang

--- resources/views/barang/show.blade.php
+++ resources/views/barang/show.blade.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/barang/show.css') }}">
     <title>Master Barang</title>
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/stok/laporan">Stok</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/transaksi/laporan">Transaksi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/lokasi/master">Lokasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/barang/master">Barang</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
