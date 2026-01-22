cde540aea3073afd9ff847de0eab46ee7db70cfb
Darren Gavriel <darrengvs.dg@gmail.com>
2026-01-22 11:25:37 +0700

    Menambahkan data di tabel barang

--- database/seeds/BarangSeeder.php
+++ database/seeds/BarangSeeder.php
@@ -15,5 +15,11 @@ class BarangSeeder extends Seeder
         Barang::create(['nama_barang' => 'BARANG 2', 'kode_barang' => 'BRG2']);
         Barang::create(['nama_barang' => 'BARANG 3', 'kode_barang' => 'BRG3']);
         Barang::create(['nama_barang' => 'BARANG 4', 'kode_barang' => 'BRG4']);
+        Barang::create(['nama_barang' => 'BARANG 5', 'kode_barang' => 'BRG5']);
+        Barang::create(['nama_barang' => 'BARANG 6', 'kode_barang' => 'BRG6']);
+        Barang::create(['nama_barang' => 'TV Polytron', 'kode_barang' => 'PSA2001']);
+        Barang::create(['nama_barang' => 'TV Sharp', 'kode_barang' => 'SSA2001']);
+        Barang::create(['nama_barang' => 'Radio Sharp', 'kode_barang' => 'SSA2002']);
+        Barang::create(['nama_barang' => 'Radio Polytron', 'kode_barang' => 'PSA2002']);
     }
 }
